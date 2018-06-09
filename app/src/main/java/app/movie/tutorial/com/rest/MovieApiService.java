bdab92bc56fc6d79a2e3a4936bb7fbda8544af06
Nathan <nathan.wang2@gmail.com>
2018-06-08 20:40:45 -0500

    Added new query and fixed model naming convention

--- app/src/main/java/app/movie/tutorial/com/rest/MovieApiService.java
+++ app/src/main/java/app/movie/tutorial/com/rest/MovieApiService.java
@@ -1,11 +1,10 @@
 package app.movie.tutorial.com.rest;
 
-import app.movie.tutorial.com.model.Movie;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 import retrofit2.Call;
-import app.movie.tutorial.com.model.MovieResponse;
+import app.movie.tutorial.com.model.MovieModel;
 
 /**
  * Created by Gino Osahon on 13/03/2017.
@@ -14,11 +13,11 @@ import app.movie.tutorial.com.model.MovieResponse;
 public interface MovieApiService {
 
     @GET("movie/top_rated")
-    Call<MovieResponse> getTopRatedMovies(@Query("api_key") String apiKey);
+    Call<MovieModel> getTopRatedMovies(@Query("api_key") String apiKey);
 
     @GET("movie/popular")
-    Call<MovieResponse> getPoplarMovies(@Query("api_key") String apiKey);
+    Call<MovieModel> getPoplarMovies(@Query("api_key") String apiKey);
 
     @GET("movie/{id}")
-    Call<MovieResponse> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
+    Call<MovieModel> getMovieDetails(@Path("id") int id, @Query("api_key") String apiKey);
 }
